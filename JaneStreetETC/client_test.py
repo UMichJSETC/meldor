b13ddabb42e640786edf9b9e314274d9250bc3a6
dennywal <dennywal@umich.edu>
2017-03-11 13:42:41 -0500

    added parser

--- JaneStreetETC/client_test.py
+++ JaneStreetETC/client_test.py
@@ -23,6 +23,23 @@ def read(exchange):
 def tradeBonds(exchange, volume, buy_sell, price, ID):
     write(exchange, {"type": "add", "order_id": ID, "symbol": "BOND", "dir": buy_sell, "price": price, "size": volume})
 
+def buyFair(fair, item, ID, volume):
+    write(exchange, {"type": "add", "order_id": ID, "symbol": item, "dir": "BUY", "price": fair - 1, "size": volume})
+    
+def sellFair(fair, item, ID, volume):
+    write(exchange, {"type": "add", "order_id": ID, "symbol": item, "dir": "SELL", "price": fair + 1, "size": volume})
+
+def parseExchange(exchange):
+    feed = read(exchange)
+    parsedFeed = json.loads(feed)
+    type = parsedFeed['type']
+    if (type == "trade"):
+        symbol = parsedFeed['symbol']
+        price = parsedFeed['price']
+        size = parsedFeed['size']
+        return symbol, price, size
+
+curr_trades = []
 if __name__ == "__main__":
     exchange = connect()
     write(exchange, {"type": "hello", "team": "MELDOR"})
@@ -30,13 +47,19 @@ if __name__ == "__main__":
     print("The exchange replied:", hello_from_exchange, file=sys.stderr)
     buyIndex = 1
     sellIndex = 2
+
+    x = time.time()
     while(True):
-        time.sleep(1)
-        tradeBonds(exchange, 1, "BUY", 999, buyIndex)
+        if (time.time() > (x + 1)):
+            tradeBonds(exchange, 1, "BUY", 999, buyIndex)
+            buyIndex = buyIndex + 2
+            tradeBonds(exchange, 1, "SELL", 1001, sellIndex)
+            sellIndex = sellIndex + 2
+            x = time.time()
+        curr_trades = parseExchange(exchange)
+        print (curr_trades)
+
         hello_from_exchange = read(exchange)
-        print("The exchange replied:", hello_from_exchange, file=sys.stderr)
-        buyIndex = buyIndex + 2
-        tradeBonds(exchange, 1, "SELL", 1001, sellIndex)
+        #print("The exchange replied:", hello_from_exchange, file=sys.stderr)
         hello_from_exchange = read(exchange)
-        print("The exchange replied:", hello_from_exchange, file=sys.stderr)
-        sellIndex = sellIndex + 2
+        #print("The exchange replied:", hello_from_exchange, file=sys.stderr)
