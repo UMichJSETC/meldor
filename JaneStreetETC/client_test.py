f69449cdea551022fcbf89ca190b21401d48f311
dennywal <dennywal@umich.edu>
2017-03-11 18:13:51 -0500

    hopefully working xlf

--- JaneStreetETC/client_test.py
+++ JaneStreetETC/client_test.py
@@ -201,10 +201,14 @@ if __name__ == "__main__":
             
             
             VALTrader(BZFair, EFair,valbz_count, vale_count)
-        if (XLF_true(bondFair, gsFair, msFair, wfcFair) > xlfFair):
+            xlfTrue = XLF_true(bondFair, gsFair, msFair, wfcFair)
+        if (xlfTrue > xlfFair):
+            print("xlfTrue happens")
             sellPackage(bondFair, gsFair, msFair, wfcFair)
+            buyFair(xlfTrue, "XLF", 2017, 1)
         else:
             buyPackage(bondFair, gsFair, msFair, wfcFair)
+            sellFair(xlfTrue, "XLF", 2018, 1)
         if (type == "fill"):
             orderID = feed['order_id']
             symbol = feed['symbol']
